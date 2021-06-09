a2e9362c0c33db175e0d17594f44dcf022e5c331
djefrey <34744225+djefrey@users.noreply.github.com>
2021-06-09 13:46:40 +0200

    use global variables instead of hard coded values

--- app.js
+++ app.js
@@ -3,7 +3,13 @@ const ctx = canvas.getContext("2d");
 
 const result = document.getElementById("result");
 
+const CENTER = {
+    x: 400,
+    y: 400,
+};
 const RADIUS = 300;
+const RING_SIZE = 25;
+
 
 /* DRAW */
 
@@ -19,14 +25,14 @@ function drawRing() {
     ctx.fillStyle = "#F00";
 
     ctx.beginPath();
-    ctx.arc(400, 400, RADIUS + 25, 0, Math.PI * 2, true);
+    ctx.arc(CENTER.x, CENTER.y, RADIUS + RING_SIZE, 0, Math.PI * 2, true);
     ctx.closePath();
     ctx.fill();
 
     ctx.fillStyle = "#FFF";
 
     ctx.beginPath();
-    ctx.arc(400, 400, RADIUS, 0, Math.PI * 2, true);
+    ctx.arc(CENTER.x, CENTER.y, RADIUS, 0, Math.PI * 2, true);
     ctx.closePath();
     ctx.fill();
 }
@@ -41,14 +47,14 @@ function drawTriangle() {
     ctx.fillStyle = "#00F";
 
     ctx.beginPath();
-    ctx.moveTo(400, 400 - RADIUS);
-    ctx.lineTo(400 + cos30, 400 + sin30);
-    ctx.lineTo(400 - cos30, 400 + sin30);
-    console.log(400 - cos30, 400 + sin30);
-    ctx.lineTo(400, 400 - RADIUS);
+    ctx.moveTo(CENTER.x, CENTER.y - RADIUS);
+    ctx.lineTo(CENTER.x + cos30, CENTER.y + sin30);
+    ctx.lineTo(CENTER.x - cos30, CENTER.y + sin30);
+    ctx.lineTo(CENTER.x, CENTER.y - RADIUS);
     ctx.closePath();
     ctx.fill();
 }
+
 
 /* CLICK */
 
@@ -69,44 +75,64 @@ function onClick(event) {
 }
 
 //
+// Return 1 if the given position is inside the ring
 // Calculate the distance between the center and the point
 // Check if the distance is between the two circles radius
 //
 function checkRingClick(pos) {
-    const diffX = 400 - pos.x;
-    const diffY = 400 - pos.y;
+    const diffX = CENTER.x - pos.x;
+    const diffY = CENTER.y - pos.y;
     const dist = Math.sqrt(diffX * diffX + diffY * diffY);
 
-    return (dist >= RADIUS && dist <= RADIUS + 25);
+    return (dist >= RADIUS && dist <= RADIUS + RING_SIZE);
 }
 
 //
-// First, check if the X coordinate correspond to the triangle base and if the click is not under the triangle
-// Then, the left edge of the triangle is considered as an affine function
+// Return 1 if the given position is inside the triangle
+// First, check if the X coordinate corresponds with the triangle base and if the click is not under the triangle
+// The left and right edges are considered as linear function
 // It calculates the a factor and b value ( f(x) = a * x + b )
-// It calculates the affine function value using the X coordinate (= height)
+// It calculates the linear function value using the X coordinate (= height)
 // It checks if the Y coordinate is superior than the height (Y increase as we go "down")
 //
 function checkTriangleClick(pos) {
-    const leftEdgeStart = {
-        x: 400 - (Math.cos(Math.PI / 6) * RADIUS),
-        y: 400 + (Math.sin(Math.PI / 6) * RADIUS),
+    const leftVertex = {
+        x: CENTER.x - (Math.cos(Math.PI / 6) * RADIUS),
+        y: CENTER.y + (Math.sin(Math.PI / 6) * RADIUS),
     };
-    const leftEdgeEnd = {
-        x: 400,
-        y: 400 - RADIUS,
+    const topVertex = {
+        x: CENTER.x,
+        y: CENTER.y - RADIUS,
+    };
+    const rightVertex = {
+        x: CENTER.x + (Math.cos(Math.PI / 6) * RADIUS),
+        y: CENTER.y + (Math.sin(Math.PI / 6) * RADIUS),
     };
 
-    if (pos.x >= 400 - RADIUS && pos.x <= 400 + RADIUS
-    && pos.y <= leftEdgeStart.y) {
-        const aFactor = (leftEdgeEnd.y - leftEdgeStart.y) / (leftEdgeEnd.x - leftEdgeStart.x);
-        const bValue = leftEdgeEnd.y - aFactor * leftEdgeEnd.x;
-
-        const height = bValue + aFactor * (pos.x <= 400
-            ? pos.x
-            : 800 - pos.x);
+    if (pos.x >= CENTER.x - RADIUS && pos.x <= CENTER.y + RADIUS
+    && pos.y <= leftVertex.y) {
+        const values = pos.x <= CENTER.x
+            ? calcLinearFctFactors(leftVertex, topVertex)
+            : calcLinearFctFactors(topVertex, rightVertex);
+        const height = values.a * pos.x + values.b;
 
         return (pos.y >= height);
     }
     return (0);
+}
+
+//
+// Calculate and return linear function factors
+// a = (y2 - y1) / (x2 - x1)
+// b = y1 - a * y1
+//
+function calcLinearFctFactors(firstVertex, secondVertex)
+{
+    const a = (secondVertex.y - firstVertex.y) / (secondVertex.x - firstVertex.x);
+    const b = firstVertex.y - a * firstVertex.x;
+
+    return ({
+        a: a,
+        b: b,
+    });
 }
